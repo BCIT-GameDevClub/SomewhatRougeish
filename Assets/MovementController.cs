9ce22b3a2d7459d9d46b57852ad71821a9204cc7
Jason Cheung <jayson.h.cheung@gmail.com>
2017-11-29 13:36:07 -0800

    Movement
    Collides with wall

---
+++ Assets/MovementController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float xVel = Input.GetAxisRaw("Horizontal") * speed;
+        float yVel = Input.GetAxisRaw("Vertical") * speed;
+
+        rb.velocity = new Vector2(xVel, yVel);  // non-normalized! diagonal movement will be faster than speed.
+    }
+
+}
